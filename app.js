3f1eeb03db9f8d92a663f47c01c8ea0fdf531922
Anwarul Haque <anwarul@kolonizer.com>
2021-06-29 13:54:21 +0530

    test

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.status(200).json({message:"Hello World"})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
